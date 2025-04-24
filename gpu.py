cc74daec3ae4f8c3a294bfe001674a77cad00dc0
Filippo Romeo <romeofilippo95@gmail.com>
2025-04-24 17:58:06 +0100

    fuckembedd

--- gpu.py
+++ gpu.py
@@ -1,5 +1,14 @@
 import torch
 
+import tensorflow as tf
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    print(f"{len(gpus)} GPU(s) available: {[gpu.name for gpu in gpus]}")
+else:
+    print("No GPUs available.")
+
+
 if torch.cuda.is_available():
     print("GPU is available.")
     print("Device:", torch.cuda.get_device_name(0))
